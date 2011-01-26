ead2ed40cd103f78624f4e0bbf934cc2cc007724
Alex Vandiver <alex@chmrr.net>
2011-01-26 04:41:32 -0500

    Make use of Config::Gitlike 1.04's ::Git, which does the heavy lifting

--- lib/Git/PurePerl/Config.pm
+++ lib/Git/PurePerl/Config.pm
@@ -4,14 +4,8 @@ use MooseX::StrictConstructor;
 use Moose::Util::TypeConstraints;
 use namespace::autoclean;
 
-extends 'Config::GitLike';
+extends 'Config::GitLike::Git';
 
-has '+confname' => ( default => "gitconfig" );
 has 'git'       => ( is => 'ro', isa => 'Git::PurePerl', required => 1, weak_ref => 1 );
-
-override dir_file => sub {
-    my $self = shift;
-    return $self->git->gitdir->file("config");
-};
 
 1;
